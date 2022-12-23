45c61ed5bfc4cf1b31eae118163082b0ca7643c8
eroul-ri <bhr139313@gmail.com>
2022-12-23 17:12:28 +0900

    잘못된 extends 제거

--- src/main/java/com/api/demo/inven/dto/InvenRequestDto.java
+++ src/main/java/com/api/demo/inven/dto/InvenRequestDto.java
@@ -1,16 +1,5 @@
 package com.api.demo.inven.dto;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+public class InvenRequestDto {
 
-public class InvenRequestDto extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
 }
